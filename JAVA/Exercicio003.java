4398908b436fac9fa1a9d4d6873cef79a1b5dad0
Cristiano José Cecanho <cecanho@gmail.com>
2025-11-30 19:14:36 -0300

    Add files via upload

---
+++ JAVA/Exercicio003.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Exercicio003 {
+	static float 	NUM1, NUM2, NUM3, NUM4, MA;
+	
+	public static void main(String[] args) {
+		NUM1 = 5;
+		NUM2 = 3;
+		NUM3 = 8;
+		NUM4 = 10;		
+		
+		MA = (NUM1 + NUM2 + NUM3 + NUM4) / 4;
+		
+		System.out.println("A media e: " + MA);
+
+	}
+
+}
