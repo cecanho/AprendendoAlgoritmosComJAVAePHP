1e9156b65bb2167bb690b87b1d6ca99d7e78acc1
Cristiano José Cecanho <cecanho@gmail.com>
2025-12-07 16:56:19 -0300

    Add files via upload

---
+++ JAVA/Exercicio004.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Exercicio004 {
+
+	public static void main(String[] args) {
+		float temperatura = 37.1f, febre = 37;
+		
+		if(temperatura >= febre) {
+			System.out.println("Paciente com FEBRE!");
+		}
+
+	}
+
+}
---
+++ JAVA/Exercicio005.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Exercicio005 {
+
+	public static void main(String[] args) {
+		int num = 17;
+
+		if((num % 2) == 0) {
+			System.out.println("O numero e par");
+		}else {
+			System.out.println("O numero e impar");
+		}
+	}
+}
